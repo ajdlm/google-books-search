e692840d53b8ca0dd61033e85e5a5020c3ebef14
Antonio de las Morenas <a.j.delasmorenas@gmail.com>
2019-11-30 13:56:08 -0600

    Fixed the problem where the Link (now NavLink) associated with the path the site was initially loaded on in the browser would always retain the active class even if the user clicked on another part of the navbar (although it persists with /search)

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,39 +1,38 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "./style.css";
 
 function Navbar() {
   return (
     <nav className="navbar navbar-expand navbar-light bg-light fixed-top py-4 border-bottom border-dark">
-      <Link className="navbar-brand" to="/">
+      <NavLink className="navbar-brand" to="/">
         Google Books Search
-      </Link>
+      </NavLink>
 
       <div>
         <ul className="navbar-nav">
           <li className="nav-item">
-            <Link
+            <NavLink
               to="/"
               className={`nav-link${
-                window.location.pathname === "/" ||
-                window.location.pathname === "/search"
-                  ? " active"
-                  : ""
+                window.location.pathname === "/search" ? " active" : ""
               }`}
+              activeClassName="active"
+              exact
             >
               Search
-            </Link>
+            </NavLink>
           </li>
 
           <li className="nav-item">
-            <Link
+            <NavLink
               to="/saved"
-              className={`nav-link${
-                window.location.pathname === "/saved" ? " active" : ""
-              }`}
+              className="nav-link"
+              activeClassName="active"
+              exact
             >
               Saved
-            </Link>
+            </NavLink>
           </li>
         </ul>
       </div>
