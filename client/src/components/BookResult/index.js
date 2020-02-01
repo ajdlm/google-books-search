9dd57c2f1ef3788eec954257a7007ff77c65dea4
Antonio de las Morenas <a.j.delasmorenas@gmail.com>
2020-02-01 13:58:42 -0600

    Gave the Booklist columns breakpoints and media queries to make them more responsive to different viewport widths

--- client/src/components/BookResult/index.js
+++ client/src/components/BookResult/index.js
@@ -5,12 +5,12 @@ const BookResult = props => {
   return (
     <div className="border-bottom border-dark p-4">
       <div className="row">
-        <div className="col-6">
+        <div className="col-sm-6 bookTitleDiv">
           <h4>
             <strong>{props.book.title}</strong>
           </h4>
         </div>
-        <div className="col-6 d-flex justify-content-end align-items-center">
+        <div className="col-sm-6 d-flex justify-content-end align-items-center buttonDiv">
           <button
             className="viewButton btn btn-danger mr-2"
             onClick={() => {
@@ -40,7 +40,7 @@ const BookResult = props => {
       </div>
 
       <div className="row mb-3">
-        <div className="col-12">
+        <div className="col-12 authorStringDiv">
           <h5 className="mb-0">{`Written by ${props.makeAuthorString(
             props.book.authors
           )}`}</h5>
@@ -48,11 +48,11 @@ const BookResult = props => {
       </div>
 
       <div className="row">
-        <div className="col-2 d-flex align-items-center">
+        <div className="col-lg-2 col-md-3 col-sm-4 d-flex align-items-center coverImageDiv">
           <img src={props.book.image} alt="book-cover" />
         </div>
 
-        <div className="col-10">
+        <div className="col-lg-10 col-md-9 col-sm-8">
           <p className="mb-0">{props.book.description}</p>
         </div>
       </div>
