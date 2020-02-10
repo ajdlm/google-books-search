2a0bafb3c623ebd7a17a48350ff12c6b0b791c3d
Antonio de las Morenas <a.j.delasmorenas@gmail.com>
2020-02-09 18:16:11 -0600

    Changed the search input placeholder so that it can be seen in its entirety even on mobile phone-sized viewports

--- client/src/components/SearchBar/index.js
+++ client/src/components/SearchBar/index.js
@@ -8,7 +8,7 @@ const SearchBar = props => {
             <h4 className="text-center mb-0 mt-2">Book Search</h4>
 
             <div className="input-group d-flex py-2 mb-2">
-                <input value={props.query} onChange={props.changeSearchInput} type="text" placeholder="Enter a book you'd like to search for." className="bookSearch flex-fill px-2 mx-3" />
+                <input value={props.query} onChange={props.changeSearchInput} type="text" placeholder="Search books..." className="bookSearch flex-fill px-2 mx-3" />
 
                 <button type="submit" onClick={props.searchForBook} className="searchButton btn btn-danger mr-3">Search</button>
             </div>
